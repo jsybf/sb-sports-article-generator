509fc8095cf8aa9c3351f445c8ccad09e7a304c6
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-04-10 02:54:54 +0900

    sample code that scrape upcoming hockey game schedule

--- src/main/Hello.kt
+++ src/main/Hello.kt
@@ -1,4 +1,40 @@
-fun main(args: Array<String>) {
-    println("Hello, World")
+import com.microsoft.playwright.Page
+import com.microsoft.playwright.Playwright
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+fun main(args: Array<String>): Unit {
+    val broswer = PlaywrightBrowser()
+
+    val links = broswer
+        .doAndGet {
+            navigate("https://www.flashscore.co.kr/hockey/")
+            locator("#live-table > div.filters > div.filters__group > div:nth-child(5) > div").click()
+        }
+        .select("section.event div.event__match > a")
+        .map { it.attribute("href")?.value }
+        .filterNotNull()
+        .onEach { println(it) }
+
+    broswer.close()
+    println("end")
 }
 
+class PlaywrightBrowser {
+    private val playwright: Playwright = Playwright.create()
+    private val browser = playwright.chromium().launch()
+
+    fun doAndGet(actions: Page.() -> Unit): Document {
+        val page = browser.newPage()
+        page.actions()
+
+        val html = page.content().let { Jsoup.parse(it) }
+        page.close()
+        return html
+    }
+
+    fun close() {
+        browser.close()
+        playwright.close()
+    }
+}
