ac00c9023280e8972cb4e6134734f3f7457e8239
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-04-19 20:05:09 +0900

    add spojoy-scraper maven module

---
+++ llm-article-writer/spojoy-scraper/src/main/Hello.kt
@@ -0,0 +1,8 @@
+package io.gitp.llmarticlewrtier.spojoyscraper
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger(object {}::class.java.packageName)
+private fun main() {
+    logger.info("this is {} package", "io.gitp.llmarticlewrtier.spojoyscraper")
+}
