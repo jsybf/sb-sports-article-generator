2df1ab54ef1e2f636f6c8789bd1a9a491c242348
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-04-21 07:50:35 +0900

    add pick-generator/scraper/scraper-base maven module

---
+++ pick-generator/scraper/scraper-base/src/main/Logger.kt
@@ -0,0 +1,5 @@
+package io.gitp.sbpick.pickgenerator.scraper.scrapebase
+
+import org.slf4j.LoggerFactory
+
+internal val logger = LoggerFactory.getLogger("pick-generator.scraper.scraper-base")
---
+++ pick-generator/scraper/scraper-base/src/main/Main.kt
@@ -0,0 +1,5 @@
+package io.gitp.sbpick.pickgenerator.scraper.scrapebase
+
+fun main() {
+    logger.info("hello world")
+}
