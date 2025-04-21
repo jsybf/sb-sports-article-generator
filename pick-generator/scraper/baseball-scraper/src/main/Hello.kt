3c984fc4c45af8fd638090e535cbe1bfc8c58f28
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-04-21 10:42:18 +0900

    add pick-generator/scraper/baseball-scraper maven module

---
+++ pick-generator/scraper/baseball-scraper/src/main/Hello.kt
@@ -0,0 +1,5 @@
+package io.gitp.sbpick.pickgenerator.scraper.baseballscraper
+
+fun main() {
+    println("hello")
+}
---
+++ pick-generator/scraper/baseball-scraper/src/main/Logger.kt
@@ -0,0 +1,5 @@
+package io.gitp.sbpick.pickgenerator.scraper.baseballscraper
+
+import org.slf4j.LoggerFactory
+
+internal val logger = LoggerFactory.getLogger("pick-generator.scraper.scraper-base")
